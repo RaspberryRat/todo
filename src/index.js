66302e895008822a95964fa3824c9eb1ca1ca59f
Raspberry Rat <raspberryratman@gmail.com>
2024-08-29 21:06:03 -0600

    can create new todo objects

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 import "./styles.css"
-import todo from "./todo.js"
+import Todo from "./todo.js"
 
 
+const newTodoBtn = document.getElementById('add-todo-submit');
 
+
+newTodoBtn.addEventListener('click', newTodo, false);
+
+function newTodo(event) {
+  event.preventDefault();
+  console.log("starting...")
+  const title = document.getElementById('title').value;
+  const description = document.getElementById('description').value;
+  const dueDate = document.getElementById('due-date').value;
+  const priority = document.querySelector('input[name="priority"]:checked').value;
+
+
+  const newTodo = new Todo(title, description, dueDate, priority);
+  console.log(newTodo)
+}
--- src/todo.js
+++ src/todo.js
@@ -1,8 +1,3 @@
-// What does a todo have?
-// Create a class and add title, description, date
-export default Todo;
-
-
 
 class Todo {
   constructor(title, description, dueDate, priority) {
@@ -12,3 +7,5 @@ class Todo {
     this.priority = priority;
   }
 }
+
+export default Todo;
