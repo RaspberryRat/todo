043c310bf884b0b177ab97e67772a295a93b30d5
Raspberry Rat <raspberryratman@gmail.com>
2024-08-29 20:13:34 -0600

    initial commit

--- src/menu.js
+++
@@ -1,8 +0,0 @@
-export { section };
-
-const section = document.createElement('section');
-const aboutDiv = document.createElement('div');
-const aboutPara = document.createElement('p');
-aboutPara.innerText = 'Here is our delicious menu';
-section.appendChild(aboutDiv);
-aboutDiv.appendChild(aboutPara);
