59d60ac0798e0b6edcf306a79f3d1204486748c6
squid <raspberryratman@gmail.com>
2024-08-30 15:43:02 -0600

    format todo key text function added

--- src/index.js
+++ src/index.js
@@ -30,11 +30,40 @@ function publishTodo(todo) {
     let keyText = document.createElement('div');
     let keyPairText = document.createElement('div');
     if (todo.hasOwnProperty(key)) {
-      keyText.innerText = `${key}:`;
+      const formattedKey = formatText(key);
+      keyText.innerText = `${formattedKey}:`;
       keyPairText.innerText = todo[key];
     }
     item.appendChild(keyText);
     item.appendChild(keyPairText);
     list.appendChild(item);
   }
+}
+
+function formatText(text) {
+  let wordIndex = [];
+  for (let i = 0; i < text.length; i++) {
+    if (text[i] === text[i].toUpperCase()) {
+      wordIndex.push(i);
+    }
+  }
+
+  if (wordIndex.length === 0) {
+    return titleCase(text);
+  } else {
+    let formattedText = text.toLowerCase();
+    let arrayText = formattedText.split('');
+
+    for (let i = wordIndex.length - 1; i >= 0; i--) {
+      const spaceLocation = wordIndex[i];
+      arrayText.splice(spaceLocation, 0, " ");
+    }
+
+    formattedText = arrayText.join('');
+    return formattedText = titleCase(formattedText);
+  }
+}
+
+function titleCase(str) {
+  return str.toLowerCase().replace(/\b\w/g, s => s.toUpperCase());
 }
